7956eced15d12c883520ef98b6eeaffb0c82d4d1
Hristina Gadzheva <hristina.gadzheva@gmail.com>
2022-01-23 21:59:18 +0200

    Add header comment in starting source file

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,5 +1,18 @@
-// minesweeper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/**
+*
+* Solution to course project # 13
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia
+University
+* Winter semester 2021/2022
+*
+* @author Hristina Gadzheva
+* @idnumber 7MIO600131
+* @compiler VC
+*
+* <"playGround.h">
+*
+*/
 #include <iostream>
 #include "playGround.h"
 
